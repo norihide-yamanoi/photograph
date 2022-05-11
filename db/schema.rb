623c8a589e7269b0311c960bd50cacbdbd87423f
norihide-yamanoi <akaitainoeyamayama@icloud.com>
2022-05-11 13:44:42 +0900

    ユニーク制約作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_11_041208) do
+ActiveRecord::Schema.define(version: 2022_05_11_042835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2022_05_11_041208) do
     t.text "profile"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
