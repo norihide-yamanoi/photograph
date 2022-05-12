bc38c99d09de5905326e687a83a3346863d5cd2b
norihide-yamanoi <akaitainoeyamayama@icloud.com>
2022-05-12 13:47:47 +0900

    ストロングパラメータ設定

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -6,11 +6,19 @@ class PicturesController < ApplicationController
     @picture = Picture.new
   end
 
+  def create
+    Picture.create(picture_params)
+    redirect_to new_picture_path
+  end
 
 
 
 
 
+  private
 
+  def picture_params
+    params.require(:picture).permit(:image, :content)
+  end
 
 end
